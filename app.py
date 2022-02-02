e46a25a747b0b3ec8226418dde801d6c15bbe0c9
Nikolajovanovski2 <jovanovskinikola2@gmail.com>
2022-02-03 00:20:30 +0100

    Letter&Char frequency in a text file

--- app.py
+++ app.py
@@ -2,16 +2,21 @@
 from class_parser import Parser
 import time
 import word_processor
+import letter_char_frequency
+
 
 def main():
     new_parser = Parser("example.txt")
     text = new_parser.parser_function()
     print(text)
 
+
+
 if __name__ == '__main__':
     main()
     start = time.time()
     print(f"Total time to execute was {time.time() - start}")
+
 
 
 
